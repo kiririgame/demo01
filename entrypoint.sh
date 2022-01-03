9a3bbb05f8d6824d02be35f7a6d295f375e0adc7
kiririgame <71005270+kiririgame@users.noreply.github.com>
2022-01-03 20:36:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,40 @@
 #!/bin/sh
-
-# Global variables
-DIR_CONFIG="/etc/v2ray"
-DIR_RUNTIME="/usr/bin"
-DIR_TMP="$(mktemp -d)"
-
-# Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+# Download and install V2Ray
+curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+mkdir /usr/bin/v2ray /etc/v2ray
+touch /etc/v2ray/config.json
+unzip /v2ray.zip -d /usr/bin/v2ray
+# Remove /v2ray.zip and other useless files
+rm -rf /v2ray.zip /usr/bin/v2ray/*.sig /usr/bin/v2ray/doc /usr/bin/v2ray/*.json /usr/bin/v2ray/*.dat /usr/bin/v2ray/sys*
+# V2Ray new configuration
+cat <<-EOF > /etc/v2ray/config.json
 {
-    "inbounds": [{
-        "port": ${PORT},
-        "protocol": "vless",
-        "settings": {
-            "clients": [{
-                "id": "${ID}"
-            }],
-            "decryption": "none"
-        },
-        "streamSettings": {
-            "network": "ws",
-            "wsSettings": {
-                "path": "${WSPATH}"
-            }
+  "inbounds": [
+  {
+    "port": ${PORT},
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "${UUID}",
+          "alterId": 64
         }
-    }],
-    "outbounds": [{
-        "protocol": "freedom"
-    }]
+      ]
+    },
+    "streamSettings": {
+      "network": "ws"
+    }
+  }
+  ],
+  "outbounds": [
+  {
+    "protocol": "freedom",
+    "settings": {}
+  }
+  ]
 }
 EOF
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
 
 # Get V2Ray executable release
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
